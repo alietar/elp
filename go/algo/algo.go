cb8c945b1a94f920f1a7ac94ea99fb661ed0e710
Anatole Lietar <ajm.lietar@gmail.com>
2026-01-02 19:02:16 +0100

    Cleanup dû au remplacement de la matrice par un struct

--- go/algo/algo.go
+++ go/algo/algo.go
@@ -1,25 +1,5 @@
 package algo
 
-import (
-	"fmt"
-)
-
-const TAILLE_MATRICE int = 5
-
-func afficherMatrice(matrice [TAILLE_MATRICE][TAILLE_MATRICE]float64) {
-	for i := 0; i < TAILLE_MATRICE; i++ {
-		for j := 0; j < TAILLE_MATRICE; j++ {
-			if matrice[i][j] == 0 {
-				fmt.Printf("       ")
-			} else {
-				fmt.Printf("%.2f ", matrice[i][j])
-			}
-		}
-
-		fmt.Printf("\n")
-	}
-}
-
 func (m *Matrix) FindNeighbors(startX, startY int) Matrix {
 	// Initialization of the matrices result and visited
 	result := make([][]float64, m.Size)
@@ -67,65 +47,4 @@ func (m *Matrix) findNeighborsRecursive(
 	}
 
 	return result
-}
-
-func TrouverVoisins(
-	matrice [TAILLE_MATRICE][TAILLE_MATRICE]float64,
-	depart_x, depart_y uint,
-) [TAILLE_MATRICE][TAILLE_MATRICE]float64 {
-	return trouverVoisinsRecursif(matrice, creerMatriceRes(), creerMatriceVisite(), depart_x, depart_y)
-}
-
-func trouverVoisinsRecursif(
-	matrice [TAILLE_MATRICE][TAILLE_MATRICE]float64,
-	res [TAILLE_MATRICE][TAILLE_MATRICE]float64,
-	visites [TAILLE_MATRICE][TAILLE_MATRICE]bool,
-	depart_x, depart_y uint,
-) [TAILLE_MATRICE][TAILLE_MATRICE]float64 {
-	x := depart_x
-	y := depart_y
-
-	if matrice[x][y] == 0 {
-		return res
-	}
-
-	visites[x][y] = true
-	res[x][y] = matrice[x][y]
-
-	if x > 0 && !visites[x-1][y] && matrice[x-1][y] != 0 {
-		res = trouverVoisinsRecursif(matrice, res, visites, x-1, y)
-	}
-
-	if x < uint(TAILLE_MATRICE-1) && !visites[x+1][y] && matrice[x+1][y] != 0 {
-		res = trouverVoisinsRecursif(matrice, res, visites, x+1, y)
-	}
-
-	if y > 0 && !visites[x][y-1] && matrice[x][y-1] != 0 {
-		res = trouverVoisinsRecursif(matrice, res, visites, x, y-1)
-	}
-	if y < uint(TAILLE_MATRICE-1) && !visites[x][y+1] && matrice[x][y+1] != 0 {
-		res = trouverVoisinsRecursif(matrice, res, visites, x, y+1)
-	}
-
-	return res
-}
-
-func creerMatriceRes() (matrice [TAILLE_MATRICE][TAILLE_MATRICE]float64) {
-	for i := 0; i < TAILLE_MATRICE; i++ {
-		for j := 0; j < TAILLE_MATRICE; j++ {
-			matrice[i][j] = 0
-		}
-	}
-
-	return
-}
-
-func creerMatriceVisite() (matrice [TAILLE_MATRICE][TAILLE_MATRICE]bool) {
-	for i := 0; i < TAILLE_MATRICE; i++ {
-		for j := 0; j < TAILLE_MATRICE; j++ {
-			matrice[i][j] = false
-		}
-	}
-
-	return
 }
--- go/algo/algo_benchmark_test.go
+++ go/algo/algo_benchmark_test.go
@@ -1,23 +1,22 @@
 package algo
 
 import (
-	"fmt"
 	"testing"
 )
 
-func BenchmarkTrouverVoisins(b *testing.B) {
-	matriceTest := [5][5]float64{
-		{341.76, 0, 0, 340.95, 0},
-		{0, 341.17, 0, 340.75, 340.50},
-		{0, 341.01, 340.79, 340.64, 0},
-		{341.16, 340.89, 0, 0, 0},
-		{341.08, 0, 340.60, 340.35, 339.97}}
-
-	var matriceRes [5][5]float64
-
-	for b.Loop() {
-		matriceRes = TrouverVoisins(matriceTest, 1, 1)
+func BenchmarkFindingNeighbors(b *testing.B) {
+	testMatrix := Matrix{
+		Size: 5,
+		Data: [][]float64{
+			{341.76, 0, 0, 340.95, 0},
+			{0, 341.17, 0, 340.75, 340.50},
+			{0, 341.01, 340.79, 340.64, 0},
+			{341.16, 340.89, 0, 0, 0},
+			{341.08, 0, 340.60, 340.35, 339.97},
+		},
 	}
 
-	fmt.Println(matriceRes)
+	for b.Loop() {
+		testMatrix.FindNeighbors(1, 1)
+	}
 }
