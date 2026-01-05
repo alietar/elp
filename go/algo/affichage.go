2338ab6ee2309fc643ca7c6082e2e1305d0586a4
solenecor <solene.corbard@gmail.com>
2026-01-05 19:39:52 +0100

    ajout de l'interface web interactive permettant d'afficher les points atteignables

---
+++ go/algo/affichage.go
@@ -0,0 +1,31 @@
+package algo
+
+import (
+	lgo "github.com/yageek/lambertgo"
+)
+
+// crée une structure Coordonnee pour stocker les coordonnées GPS
+type Coordonnee struct {
+	Lat float64 `json:"lat"` // gère l'export en JSON
+	Lng float64 `json:"lng"`
+}
+
+// Convertit des coordonnées Lambert93 en coordonnées GPS WGS84
+func FromLambert93ToGpsWgs84(x, y float64) (float64, float64, error) {
+
+	// Crée le point avec les coordonnées Lambert93
+	point := &lgo.Point{
+		X:    x,
+		Y:    y,
+		Z:    0,
+		Unit: lgo.Meter, // Préciser que c'est en mètres
+	}
+
+	point.ToWGS84(lgo.Lambert93) // Conversion Lambert-93 → WGS84
+
+	// Convertion de résultat en Radians vers des Degrés pour Leaflet de JS
+	point.ToDegree()
+
+	// point.Y est la Latitude, point.X est la Longitude
+	return point.Y, point.X, nil
+}
--- go/main.go
+++ go/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+<<<<<<< HEAD
 	"sync"
 	"flag"
 	"slices"
+=======
+	"html/template"
+	"net/http"
+	"os/exec"
+	"runtime"
+	"time"
+>>>>>>> 5132283 (ajout de l'interface web interactive permettant d'afficher les points atteignables)
 
 	"github.com/alietar/elp/go/algo"
 	"github.com/alietar/elp/go/findfiles"
@@ -14,6 +23,30 @@ import (
 
 func main() {
 	var exploredTilesPath []string
+=======
+// openBrowser sert à détecter le système d'exploitation et ouvrir le navigateur par défaut avec l'URL donnée
+func openBrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		fmt.Println("Erreur lors de l'ouverture du navigateur:", err)
+	}
+}
+
+func main() {
+	longitude := "4.87226"
+	latitude := "45.783844"
+>>>>>>> 5132283 (ajout de l'interface web interactive permettant d'afficher les points atteignables)
 
 	longPtr := flag.String("long", "4.582209", "starting longitude")
 	latPtr := flag.String("lat", "45.430121", "starting latitude")
@@ -201,3 +234,61 @@ func getMatrixFromLambert(xLambert, yLambert float64, path string) algo.Matrix {
 
 	return fullMatrix
 }
+=======
+	fmt.Printf("Altitude : %f\n", mat[x][y])
+
+	fullMatrix = fullMatrix.FindNeighbors(x, y) // On trouve tous les points connectés à la case de départ
+
+	xll, yll, _, _ := findfiles.ReadCoordinateLambert93File(path) // On lit les coordonnées en Lambert93 du coin inférieur gauche de la matrice
+
+	var pointsAffiche []algo.Coordonnee // Liste des points à afficher
+
+	// Parcours de la matrice pour récupérer les coordonnées des points connectés
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			if fullMatrix.Data[i][j] != 0 {
+				X := xll + float64(j)*25.0                                                 // Conversion de l'indice de colonne en coordonnée Lambert93
+				Y := (yll + 25000.0) - float64(i)*25.0                                     // Conversion de l'indice de ligne en coordonnée Lambert93
+				lat, lng, _ := algo.FromLambert93ToGpsWgs84(X, Y)                          // Conversion en coordonnées GPS WGS84
+				pointsAffiche = append(pointsAffiche, algo.Coordonnee{Lat: lat, Lng: lng}) // Ajout à la liste des points à afficher
+			}
+		}
+	}
+
+	// Lance un serveur web
+	http.HandleFunc("/points", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json") // On indique que la réponse sera au format JSON
+		json.NewEncoder(w).Encode(pointsAffiche)           // On encode la liste des points en JSON et on l'envoie en réponse
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("carte.html") // On charge le fichier HTML
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// On prépare les données à injecter dans le template
+		data := struct {
+			Lat string
+			Lng string
+		}{
+			Lat: latitude,
+			Lng: longitude,
+		}
+		tmpl.Execute(w, data) // On exécute le template carte.html avec les données
+	})
+
+	fmt.Println("Calcul terminé. Préparation de la carte...")
+
+	// On lance l'ouverture automatique dans un fil séparé
+	go func() {
+		// On attend 1 seconde pour être sûr que le serveur est bien prêt
+		time.Sleep(1 * time.Second)
+		openBrowser("http://localhost:8080") // On ouvre le navigateur par défaut avec l'URL du serveur
+	}()
+
+	http.ListenAndServe(":8080", nil) // On démarre le serveur web sur le port 8080
+
+}
+>>>>>>> 5132283 (ajout de l'interface web interactive permettant d'afficher les points atteignables)
