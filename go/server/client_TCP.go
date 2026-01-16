e75fd3490bf4d496ccbc99bd95d46d3663f47c7a
leopoldcst <leopoldconstant15@gmail.com>
2026-01-16 10:59:24 +0100

    Update client_TCP.go

--- go/server/client_TCP.go
+++ go/server/client_TCP.go
@@ -1,1 +1,27 @@
+package server
+
+import (
+	"bufio"
+	"encoding/json"
+	"net"
+)
+
+// ReceiveTCP se connecte à un serveur TCP et décode un JSON dans target
+func ReceiveTCP(address string, target any) error {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	// Lecture jusqu'au délimiteur (\n)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal([]byte(line), target)
+}
 
