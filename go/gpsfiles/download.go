574fc361b200d0813f16a310109b92da70585a45
alietar <ajm.lietar@gmail.com>
2026-01-22 11:10:03 +0100

    Added accuracy feature on download

--- go/gpsfiles/download.go
+++ go/gpsfiles/download.go
@@ -2,43 +2,41 @@ package gpsfiles
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"path"
 	"strconv"
 	"strings"
 	"time"
 )
 
-type MapAccuracy uint8
-type DepartmentNb uint8
+type MapAccuracy string
 
 const (
-	ACCURACY_1 MapAccuracy = iota
-	ACCURACY_5
-	ACCURACY_25
+	ACCURACY_1  MapAccuracy = "1M"
+	ACCURACY_5  MapAccuracy = "5M"
+	ACCURACY_25 MapAccuracy = "25M"
 )
 
-type Social struct {
-	Entries []Entry `xml:"entry"`
+type Link struct {
+	Href string `xml:"href,attr"`
+	Type string `xml:"type,attr"`
 }
 
 type Entry struct {
-	Url     string `xml:"id"`
-	Content string `xml:"content"`
+	Link Link `xml:"link"`
 }
 
-func FetchingDepartmentUrl(nb DepartmentNb, accuracy MapAccuracy) {
-
-	// var users Users
-	// we unmarshal our byteArray which contains our
-	// xmlFiles content into 'users' which we defined above
-	// xml.Unmarshal(byteValue, &users)
+type Feed struct {
+	Entries []Entry `xml:"entry"`
 }
 
-func computeFirstURL(nb DepartmentNb, accuracy MapAccuracy) (url string) {
+func computeCapabilitiesURL(nb int, accuracy MapAccuracy) (url string) {
 	// Motif spécifique au département dans l'URL (ex: D069 pour le rhône)
 	depart := fmt.Sprintf("D%03d", nb)
 
@@ -51,110 +49,42 @@ func computeFirstURL(nb DepartmentNb, accuracy MapAccuracy) (url string) {
 	return
 }
 
-// func fetchDataURL(url string) (dataUrl string) {
-// 	client := &http.Client{}
-// 	req, _ := http.NewRequest("GET", url, nil)
-// 	req.Header.Set("Accept", "application/atom+xml") // On force le format XML
-// 	req.Header.Set("User-Agent", "Mozilla/5.0")      // On simule un navigateur
-//
-// 	rep, err := client.Do(req)
-//
-// 	if err != nil {
-// 		fmt.Println("impossible de joindre l'URL :", err)
-// 		return ""
-// 	}
-// 	defer rep.Body.Close()
-//
-// 	data, err := io.ReadAll(rep.Body) // data contient le contenu du fichier sous forme de byte
-// 	if err != nil {
-// 		fmt.Println("erreur lecture catalogue :", err)
-// 		return ""
-// 	}
-//
-// 	// contenu := string(data)
-//
-// }
-
-func downloadUnizpFile(path string) {
-
-}
-
-// prend en paramètres le numéro de département et télécharge la base de données correspondant
-func DownloadUnzipDB(depart int) string {
-	fmt.Printf("Starting download procedure for department %02d\n", depart)
-
-	err := os.MkdirAll("db", 0755) // Assure que le dossier "bd" existe (le crée si nécessaire) ; err est nil si le dossier est prêt
-
-	if err != nil {
-		fmt.Println("Error while creating the DB folder:", err)
-		return ""
-	}
-
-	motif := fmt.Sprintf("D%03d", depart) // Motif spécifique au département dans l'URL (ex: D069 pour le rhône), 03d signifie 3 chiffres (zéro padding devant si nécessaire)
-
-	apiURL := fmt.Sprintf("https://data.geopf.fr/telechargement/resource/BDALTI?zone=%s", motif)
-
-	// Pour récupérer les headers
+func fetchXMLFromUrl(url string) (feed Feed) {
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", apiURL, nil)
+	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("Accept", "application/atom+xml") // On force le format XML
 	req.Header.Set("User-Agent", "Mozilla/5.0")      // On simule un navigateur
 
 	rep, err := client.Do(req)
 
 	if err != nil {
-		fmt.Println("impossible de joindre l'URL :", err)
-		return ""
+		fmt.Println("impossible de joindre l'URL :")
+		log.Fatal(err)
 	}
+
 	defer rep.Body.Close()
 
-	data, err := io.ReadAll(rep.Body) // data contient le contenu du fichier sous forme de byte
+	decoder := xml.NewDecoder(rep.Body)
+
+	if err := decoder.Decode(&feed); err != nil {
+		log.Fatalf("Erreur lors du parsing XML : %v", err)
+	}
+
+	return
+}
+
+func downloadFromURL(url string, filename string) {
+	out, err := os.Create(filename) // Création du fichier local pour écrire les données téléchargées
 	if err != nil {
-		fmt.Println("erreur lecture catalogue :", err)
-		return ""
-	}
-
-	contenu := string(data)
-
-	// Dans le XML, on cherche le contenu entre <title>...</title> situé à l'intérieur de la balise <entry>
-
-	// On cherche l'entrée
-	posEntry := strings.Index(contenu, "<entry>")
-	if posEntry == -1 {
-		fmt.Println("Aucune entrée trouvée pour ce département")
-		return ""
-	}
-
-	reste := contenu[posEntry:] // On travaille à partir de la position de l'entrée
-	startTag := "<title>"
-	endTag := "</title>"
-
-	// On cherche le titre à l'intérieur de cette entrée
-	posStart := strings.Index(reste, startTag) + len(startTag)
-	posEnd := strings.Index(reste, endTag)
-
-	if posStart == -1 || posEnd == -1 {
-		fmt.Println("Impossible d'extraire le nom de la ressource")
-		return ""
-	}
-
-	subResource := reste[posStart:posEnd] // Nom de la ressource à télécharger
-	nomFichier := subResource + ".7z"
-	finalURL := fmt.Sprintf("https://data.geopf.fr/telechargement/download/BDALTI/%s/%s", subResource, nomFichier)
-
-	out, err := os.Create(nomFichier) // Création du fichier local pour écrire les données téléchargées
-	if err != nil {
-		fmt.Println("erreur création fichier :", err)
-		return ""
+		fmt.Println("erreur création fichier :")
+		log.Fatal(err)
 	}
 
 	defer out.Close() // Assure la fermeture du fichier à la fin de la fonction (uniquement par sécurité car on le fait manuellement après)
 
-	fmt.Printf("Download started %s\n", nomFichier)
-
 	// On réutilise le client
 	clientDownload := &http.Client{}
-	reqDownload, _ := http.NewRequest("GET", finalURL, nil)
+	reqDownload, _ := http.NewRequest("GET", url, nil)
 
 	// On simule un navigateur pour passer l'erreur 403
 	reqDownload.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
@@ -162,51 +92,109 @@ func DownloadUnzipDB(depart int) string {
 
 	fichier, err := clientDownload.Do(reqDownload) // Téléchargement du fichier
 	if err != nil {
-		fmt.Println("erreur téléchargement fichier :", err)
-		return ""
+		fmt.Println("erreur téléchargement fichier :")
+		log.Fatal(err)
 	}
+
 	defer fichier.Body.Close()
 
 	if fichier.StatusCode != http.StatusOK { // Vérification du code de statut HTTP
 		fmt.Println("erreur serveur :", fichier.Status)
-		return ""
+		log.Fatal("Wrong status code")
 	}
 
-	_, err = io.Copy(out, fichier.Body) // Utilisation de io.Copy pour ne pas saturer la RAM (flux direct vers disque)
-	if err != nil {
-		fmt.Println("erreur pendant l'écriture :", err)
-		return ""
+	counter := &WriteCounter{
+		Size: uint64(fichier.ContentLength),
 	}
 
-	fmt.Printf("Download finished\n")
+	// 4. Copier les données
+	// io.TeeReader divise le flux: il lit resp.Body, écrit dans 'counter',
+	// et retourne les données pour qu'elles soient écrites dans 'out' par io.Copy.
+	if _, err = io.Copy(out, io.TeeReader(fichier.Body, counter)); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(" | \033[32mDownload finished\033[0m")
 
 	out.Close() // On ferme le fichier avant de le décompresser
-
-	// Décompression du fichier .7z
-	fmt.Println("Decompressing the file...")
-
-	cmd := exec.Command("7zz", "e", nomFichier, "-odb", "-y", "-r", "*.asc") // commande pour extraire uniquement les fichiers .asc dans le dossier bd (e pour extraire, o pour choisir le répertoire, y pour répondre oui à toutes les questions si besoin, r pour parcourir tous les sous-dossiers du .7z)
-
-	// On lance la commande et on attend la fin
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println("Erreur lors de la décompression du fichier :", err)
-		return ""
-	}
-	os.Remove(nomFichier) // On supprime le fichier .7z après extraction
-	fmt.Println("Extraction finished")
-
-	return ""
-
 }
 
-func DownloadAllDepartements() {
+func unzip(zipPath string, outputFolder string) {
+	// Décompression du fichier .7z
+	fmt.Print("└─> Decompressing the file...")
+
+	cmd := exec.Command("7zz", "e", zipPath, "-o"+outputFolder, "-y", "-r", "*.asc") // commande pour extraire uniquement les fichiers .asc dans le dossier bd (e pour extraire, o pour choisir le répertoire, y pour répondre oui à toutes les questions si besoin, r pour parcourir tous les sous-dossiers du .7z)
+
+	// On lance la commande et on attend la fin
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Erreur lors de la décompression du fichier :")
+		log.Fatal(err)
+	}
+
+	os.Remove(zipPath) // On supprime le fichier .7z après extraction
+	fmt.Println(" | \033[32mExtraction finished\033[0m")
+}
+
+func DownloadUnzipDepartment(nb int, accuracy MapAccuracy) {
+	fmt.Printf("Starting download procedure for department n°\033[1m%02d\033[0m at \033[1m%s\033[0m\n", nb, string(accuracy))
+
+	// Finding the ressource URL
+	capabilitiesURL := computeCapabilitiesURL(nb, accuracy)
+
+	var resourcesURL string
+
+	for _, entry := range fetchXMLFromUrl(capabilitiesURL).Entries {
+		if strings.Contains(entry.Link.Href, "1M") && accuracy == ACCURACY_1 {
+			resourcesURL = entry.Link.Href
+			break
+		} else if strings.Contains(entry.Link.Href, "5M") && accuracy == ACCURACY_5 {
+			resourcesURL = entry.Link.Href
+			break
+		} else if strings.Contains(entry.Link.Href, "25M") && accuracy == ACCURACY_25 {
+			resourcesURL = entry.Link.Href
+			break
+		}
+	}
+
+	if resourcesURL == "" {
+		log.Fatal("Didn't find the accuracy in the capabilities")
+	}
+
+	// Finding the download URLs
+	var downloadURLs []string
+
+	for _, entry := range fetchXMLFromUrl(resourcesURL).Entries {
+		if entry.Link.Type == "application/x-7z-compressed" {
+			downloadURLs = append(downloadURLs, entry.Link.Href)
+		}
+	}
+
+	if len(downloadURLs) == 0 {
+		log.Fatal("No ressources available for this accuracy")
+	}
+
+	err := os.MkdirAll("db", 0755) // Assure que le dossier "bd" existe (le crée si nécessaire) ; err est nil si le dossier est prêt
+
+	if err != nil {
+		log.Fatalf("Error while creating the DB folder: %v", err)
+	}
+
+	for _, url := range downloadURLs {
+		filename := path.Base(url)
+		downloadFromURL(url, filename)
+		unzip(filename, "./db/"+string(accuracy))
+		// unzip(filename, "./db/"+string(accuracy)+"/"+strconv.Itoa(nb))
+	}
+}
+
+func DownloadAllDepartements(accuracy MapAccuracy) {
 	for compteur := 1; compteur < 96; compteur++ { // On ne s'occupe que des départements métropolitains (1 à 95)
 		if compteur == 20 { // On ignore le 20 car c'est la Corse (2A et 2B)
 			continue
 		}
 
-		DownloadUnzipDB(compteur)
+		DownloadUnzipDepartment(compteur, accuracy)
 	}
 }
 
@@ -239,4 +227,29 @@ func GetDepartement(x, y float64) int {
 		return -1
 	}
 	return num
+}
+
+// // Used to see progress of download
+type WriteCounter struct {
+	Total uint64 // Bytes already downloaded
+	Size  uint64 // Total amount to download
+}
+
+func (wc *WriteCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	wc.Total += uint64(n)
+	wc.PrintProgress()
+	return n, nil
+}
+
+func (wc *WriteCounter) PrintProgress() {
+	// Deletes line and go to begining
+	fmt.Printf("\r%s", strings.Repeat(" ", 35))
+
+	percent := float64(wc.Total) / float64(wc.Size) * 100
+
+	fmt.Printf("\r└─> Downloading... %.2f%% (%.2f MB / %.2f MB)",
+		percent,
+		float64(wc.Total)/1024/1024,
+		float64(wc.Size)/1024/1024)
 }
--- go/main.go
+++ go/main.go
@@ -11,11 +11,20 @@ import (
 )
 
 func main() {
-	dlAll, dlSome := flagHandler()
+	fmt.Println("\n        _,--',   _._.--._____")
+	fmt.Println(" .--.--';_'-.', \";_      _.,-'")
+	fmt.Println(".'--'.  _.'    {`'-;_ .-.>.'")
+	fmt.Println("      '-:_      )  / `' '=.")
+	fmt.Println("        ) >     {_/,     /~)")
+	fmt.Println("        |/               `^ .'")
 
-	downloadDepartment(dlAll, dlSome)
+	fmt.Println("\n\033[34m --- FIND REACHABLE ---\033[0m\n")
 
-	fmt.Println("\n\033[34m --- Starting the server --- \033[0m\n")
+	dlAll, dlSome, accuracy := flagHandler()
+
+	downloadDepartments(dlAll, dlSome, accuracy)
+
+	fmt.Println("\n\033[34m -> Starting the server\033[0m\n")
 	server.Start()
 }
 
@@ -27,10 +36,12 @@ func isThereDBFolder() bool {
 	}
 }
 
-func flagHandler() (bool, bool) {
+func flagHandler() (bool, bool, gpsfiles.MapAccuracy) {
 	downloadAllFlagPtr := flag.Bool("dl-all", false, "Downloads all the db")
-	downloadSomeFlagPtr := flag.Bool("dl-some", false, "Downloads only the provided departement")
-	forceDownloadFlagPtr := flag.Bool("dl-force", false, "Forces the download even if a db folder exists")
+	downloadSomeFlagPtr := flag.Bool("dl-some", false, "Downloads only the provided departement. Put them after all the flags!")
+	accuracy1FlagPtr := flag.Bool("accuracy-1", false, "1M Accuracy")
+	accuracy5FlagPtr := flag.Bool("accuracy-5", false, "5M Accuracy")
+	accuracy25FlagPtr := flag.Bool("accuracy-25", false, "25M Accuracy")
 
 	flag.Parse()
 
@@ -40,23 +51,45 @@ func flagHandler() (bool, bool) {
 		os.Exit(3)
 	}
 
-	if (*downloadAllFlagPtr || *downloadSomeFlagPtr) && isThereDBFolder() && !*forceDownloadFlagPtr {
-		fmt.Println("You want to download the DB but a db folder already exists,")
-		fmt.Println("to overwrite the folder, use the -dl-force flag")
-		os.Exit(3)
-	}
-
 	if !*downloadAllFlagPtr && !*downloadSomeFlagPtr && !isThereDBFolder() {
-		fmt.Println("No DB folder found, please download the DB with the  -dl-all or -dl-some flag")
+		fmt.Println("No DB folder found, please download the DB with the -dl-all or -dl-some, and -dl-accuracy flag")
 		os.Exit(3)
 	}
 
-	return *downloadAllFlagPtr, *downloadSomeFlagPtr
+	var accuracy gpsfiles.MapAccuracy
+
+	if *downloadAllFlagPtr || *downloadSomeFlagPtr {
+		if !*accuracy1FlagPtr && !*accuracy5FlagPtr && !*accuracy25FlagPtr {
+			fmt.Println("Please indicate at which accuracy you want to download")
+			fmt.Println("the file with the flags -accuracy-1, -accuracy-5 or -accuracy-25")
+			os.Exit(3)
+		}
+
+		if *downloadAllFlagPtr && *accuracy1FlagPtr {
+			fmt.Println("Downloading at 1M the whole France is too much, use -dl-some")
+			os.Exit(3)
+		}
+
+		if *accuracy1FlagPtr {
+			accuracy = gpsfiles.ACCURACY_1
+		} else if *accuracy5FlagPtr {
+			accuracy = gpsfiles.ACCURACY_5
+		} else if *accuracy25FlagPtr {
+			accuracy = gpsfiles.ACCURACY_25
+		} else {
+			fmt.Println("Invalid accuracy, use either 1, 5 or 25")
+			os.Exit(3)
+		}
+	}
+
+	return *downloadAllFlagPtr, *downloadSomeFlagPtr, accuracy
 }
 
-func downloadDepartment(dlAll, dlSome bool) {
+func downloadDepartments(dlAll, dlSome bool, accuracy gpsfiles.MapAccuracy) {
 	if dlAll {
-		gpsfiles.DownloadAllDepartements()
+		gpsfiles.DownloadAllDepartements(accuracy)
+
+		fmt.Println("\n\033[32m\033[1mSuccessfully downloaded requested departments\033[0m")
 	} else if dlSome {
 		if len(flag.Args()) == 0 {
 			fmt.Println("No department specified, please specify which department you want to download")
@@ -84,7 +117,9 @@ func downloadDepartment(dlAll, dlSome bool) {
 		}
 
 		for _, nb := range departementsNb {
-			gpsfiles.DownloadUnzipDB(nb)
+			gpsfiles.DownloadUnzipDepartment(nb, accuracy)
 		}
+
+		fmt.Println("\n\033[32m\033[1mSuccessfully downloaded requested departments\033[0m")
 	}
 }
