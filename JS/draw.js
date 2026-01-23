23ef0276932d996c7d55edddd4cb74df7b21f52e
solenecor <solene.corbard@gmail.com>
2026-01-23 15:45:55 +0100

    creation of the function to draw cards and took care of the import of global variables

---
+++ JS/draw.js
@@ -0,0 +1,18 @@
+import {packet, cardsTypes} from './game_init.js';
+
+function draw_card(packet, cardsTypes) {
+
+    if (cardsTypes.length === 0) 
+        return null;
+
+    const index = Math.floor(Math.random() * cardsTypes.length);
+    const card = cardsTypes[index];
+    const cardData = packet.get(card);
+    cardData.quantity -= 1;    
+    if (cardData.quantity === 0){
+        const i = cardsTypes.indexOf(card);
+        cardsTypes.splice(i, 1);
+    }
+    return card;
+}
+
