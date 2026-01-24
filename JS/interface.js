5a999d6802c32e2414a8b284cdaeeabefbb4874f
leopold_cst <leopoldconstant15@gmail.com>
2026-01-24 18:07:50 +0100

    reecriture de l'interface dans une classe

--- JS/interface.js
+++ JS/interface.js
@@ -1,65 +1,72 @@
 import inquirer from 'inquirer';
-import {draw_card} from './draw.js';
-import {packet, cardsTypes} from './game_init.js';
+import { draw_card } from './draw.js';
+import { packet, cardsTypes } from './game_init.js';
 
-var playersNames = []
+class Interface {
+  constructor() {
+    this.playersNames = [];
+  }
 
-async function name_player() {
-  const pseudo = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'playerName',
-      message: "What's your name?",
+  async namePlayer() {
+    const pseudo = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'playerName',
+        message: "What's your name?",
+      },
+    ]);
+
+    console.log('Hi', pseudo.playerName, ', welcome to the game!');
+    this.playersNames.push(pseudo.playerName);
+  }
+
+  async play() {
+    const play = await inquirer.prompt([
+      {
+        type: 'rawlist',
+        name: 'choice',
+        message: 'Chose your move :',
+        choices: ['Flip a card', 'Stop'],
+      },
+    ]);
+
+    if (play.choice === 'Flip a card') {
+      const card = draw_card(packet, cardsTypes);
+      console.log('You drew a', card);
+      if (packet.get(card).type === 'action' && card !== 'Second Chance') {
+        await this.action();
+      }
+    } else {
+      console.log('You won', '', 'points');
     }
-  ]);
+  }
 
-  console.log("Hi",pseudo.playerName,", welcome to the game!");
-  playersNames.push(pseudo.playerName)
-  
-}
+  async action() {
+    const action = await inquirer.prompt([
+      {
+        type: 'rawlist',
+        name: 'choice',
+        message: 'You drew an action card, who do you want to target?',
+        choices: this.playersNames,
+      },
+    ]);
 
-
-async function play() {
-  const play = await inquirer.prompt([
-    {
-      type: 'rawlist',
-      name: 'choice',
-      message: 'Chose your move :',
-      choices: ["Flip a card", "Stop"]
+    if (action.choice === this.playersNames[0]) {
+      console.log('You chose', this.playersNames[0]);
+    } else if (action.choice === this.playersNames[1]) {
+      console.log('You chose', this.playersNames[1]);
+    } else {
+      console.log('');
     }
-  ]);
+  }
 
-  if (play.choice === "Flip a card") {
-    const card = draw_card(packet, cardsTypes)
-    console.log("You drew a", card);
-    if (packet.get(card).type === 'action' && card !== 'Second Chance'){
-      await action()
-    }
-  } else {
-    console.log("You won", "" ,"points");
-    
+  async start() {
+    await this.namePlayer();
+    await this.play();
   }
 }
 
-async function action() {
-  const action = await inquirer.prompt([
-    {
-      type: 'rawlist',
-      name: 'choice',
-      message: 'You drew an action card, who do you want to target?',
-      choices: playersNames
-    }
-  ]);
+const ui = new Interface();
+await ui.start();
 
-  if (action.choice === playersNames[0]) {
-    console.log("You chose", playersNames[0]);
-  } else if (action.choice === playersNames[1]) {
-    console.log("You chose", playersNames[1]);
-    
-  } else {
-    console.log("")
-  }
-}
-
-await name_player()
-await play()
+export { Interface };
